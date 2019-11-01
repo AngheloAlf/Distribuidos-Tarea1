288861a36f2800efd7cc109b1993a86b169443d4
vicente jose saona urmeneta <vicente.saona@sansano.usm.cl>
2019-11-01 13:19:29 -0300

    Clientes que escuchan, servidor que habla

--- src/client.java
+++ src/client.java
@@ -1,11 +1,34 @@
+import java.io.*;
+import java.net.*;
+
 class client
 {
+	private static int PORT = 50000;
+	private static String IP = "230.0.0.0";
+
 	public static void main(String args[])
 	{
 		if(args.length < 1) {
 			System.out.println("ERROR: Debes especificar la IP!");
 			System.out.println("Uso: java client <ip multicast>");
 		}
-		System.out.println("Hola Alf");
+		byte[] buffer = new byte[1024];
+		try {
+
+			MulticastSocket socket = new MulticastSocket(PORT);
+			InetAddress group = InetAddress.getByName(IP);
+			socket.joinGroup(group);
+
+			while(true){
+				Thread.sleep(1000);
+				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+				socket.receive(packet);
+				String message = new String(packet.getData(), packet.getOffset(), packet.getLength());
+				System.out.println("el mensaje de Multicast recibido dice: " + message);
+			}
+		} catch(Exception e) {
+			System.out.println(e.toString());
+		}
+
 	}
 }
--- src/server.java
+++ src/server.java
@@ -1,11 +1,33 @@
+import java.io.*;
+import java.net.*;
+
 class server
 {
+
+	private static int PORT = 50000; // Must be inside range [49152 - 65535]
+	private static String IP = "230.0.0.0";
+
+	public static void sendUDPMessage(String message, String ipAddress) throws Exception
+	{
+		DatagramSocket socket = new DatagramSocket();
+		InetAddress group = InetAddress.getByName(ipAddress);
+		byte[] messageBytes = message.getBytes();
+		DatagramPacket packet = new DatagramPacket(messageBytes, messageBytes.length, group, PORT);
+		socket.send(packet);
+		socket.close();
+	}
+
 	public static void main(String args[])
 	{
 		if(args.length < 1) {
 			System.out.println("ERROR: Debes especificar la IP!");
 			System.out.println("Uso: java server <ip multicast>");
 		}
+		try {
+			sendUDPMessage("Hola Anghelo", IP);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		} 
 		System.out.println("Hola Alf");
 	}
 }
