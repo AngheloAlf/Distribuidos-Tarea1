c2a621cd6f869a47424fb90577bea9e6c218bf6e
vicente jose saona urmeneta <vicente.saona@sansano.usm.cl>
2019-12-05 13:00:28 -0300

    Se puede hacer play, pause, keewih

--- src/server.java
+++ src/server.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.NoSuchElementException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,25 +12,24 @@ class Server
 	private static int PORT = 50000; // Must be inside range [49152 - 65535]
 	private static String IP = "230.0.0.0";
 	private static Queue<Cancion> queue = new LinkedList<>();
-	volatile private static String status = "running"; //stopped, paused or playing
+	volatile private static String status = "playing"; //stopped, paused or playing
 	volatile private static String nowPlaying = null;
 	volatile private static Integer timeLeft = 0;
 	private static Integer totalSongTime = 0;
 
 	private static Pattern clientPattern = Pattern.compile("Client.*", Pattern.CASE_INSENSITIVE);
 	private static Pattern pausePattern = Pattern.compile("Client.*_PAUSE_.*", Pattern.CASE_INSENSITIVE);
-	private static Pattern keewihPattern = Pattern.compile("Client.*_KEEWIH_.*", Pattern.CASE_INSENSITIVE);
 	private static Pattern listPattern = Pattern.compile("Client.*_LIST_.*", Pattern.CASE_INSENSITIVE);
 	private static Pattern stopPattern = Pattern.compile("Client.*_STOP_.*", Pattern.CASE_INSENSITIVE);
 	private static Pattern nextPattern = Pattern.compile("Client.*_NEXT_.*", Pattern.CASE_INSENSITIVE);
 	private static Pattern historyPattern = Pattern.compile("Client.*_HISTORY_.*", Pattern.CASE_INSENSITIVE);
 
-	private static Pattern playPattern = Pattern.compile(".*?_PLAY_(.*?)_(.*?)_(.*)", Pattern.CASE_INSENSITIVE);
-	private static Pattern queuePattern = Pattern.compile(".*?_KEEWIH_(.*?_.*?)_(.*?)", Pattern.CASE_INSENSITIVE);
+	private static Pattern playPattern = Pattern.compile(".*?_PLAY_(.*?)_(\\d+?)_(.*)", Pattern.CASE_INSENSITIVE);
+	private static Pattern keewihPattern = Pattern.compile(".*?_KEEWIH_(.*?)_(\\d+?)_(.*?)", Pattern.CASE_INSENSITIVE);
 	private static Pattern jumpPattern = Pattern.compile(".*?_JUMP_(\\d+)_(.*?)", Pattern.CASE_INSENSITIVE);
 
 
-	private static Cancion cancion = new Cancion();
+	private static Cancion cancion;
 
 	public static void sendUDPMessage(String message, String ipAddress) throws Exception
 	{
@@ -62,12 +62,21 @@ class Server
 					while(true) {
 						try {
 							Thread.sleep(1000);
-							if(status.equals("running")) {
+							if(status.equals("playing")) {
 								sendUDPMessage(nowPlaying + "_" + timeLeft.toString(), IP);
+								timeLeft--;
 							} else {
 								sendUDPMessage(status, IP);
 							}
-							timeLeft--;
+							if(timeLeft == 0) {
+								try {
+									cancion = queue.remove();
+									nowPlaying = cancion.nombre;
+									timeLeft = cancion.timeLeft;
+								} catch (NoSuchElementException e) {
+									status = "stopped";
+								}
+							}
 						} catch (Exception e) {
 							System.out.println("Se ha hallado un error en el cantador:");
 							System.out.println(e.toString());
@@ -83,13 +92,16 @@ class Server
 				if(clientPattern.matcher(message).matches()) { // es un mensaje del cliente
 					System.out.print("Mensaje recibido por el servidor: ");
 					System.out.println(message);
-					if(playPattern.matcher(message).matches()) {
+					Matcher playMatch = playPattern.matcher(message);
+					Matcher keewihMatch = keewihPattern.matcher(message);
+					Matcher jumpMatch = jumpPattern.matcher(message);
+					if(playMatch.matches()) {
 						System.out.println("ALOHA");
 						while(queue.size() != 0) queue.remove(); // vaciamos la cola (Esta bien vaciarla?)
 						System.out.println("ALO1");
-						nowPlaying = playPattern.matcher(message).group(1); // Parece que esto de los grupos me tira error
+						nowPlaying = playMatch.group(1); // Parece que esto de los grupos me tira error
 						System.out.println("ALO2");
-						timeLeft = Integer.parseInt(playPattern.matcher(message).group(2));
+						timeLeft = Integer.parseInt(playMatch.group(2));
 						totalSongTime = timeLeft;
 						status = "playing";
 						System.out.println("ALO3");
@@ -100,10 +112,15 @@ class Server
 						totalSongTime = 0;
 						status = "stopped";
 					} else if(pausePattern.matcher(message).matches()) {
-						status = "paused";
-					} else if(keewihPattern.matcher(message).matches()) {
-						cancion.nombre = queuePattern.matcher(message).group(1);
-						cancion.timeLeft = Integer.parseInt(queuePattern.matcher(message).group(2));
+						if(status.equals("paused")) {
+							status = "playing";
+						} else {
+							status = "paused";
+						}
+					} else if(keewihMatch.matches()) {
+						cancion = new Cancion();
+						cancion.nombre = keewihMatch.group(1);
+						cancion.timeLeft = Integer.parseInt(keewihMatch.group(2));
 						queue.add(cancion);
 					} else if(listPattern.matcher(message).matches()) {
 						sendUDPMessage("Esta es la lista de canciones", IP); // Mostrar la lista de canciones
@@ -112,8 +129,8 @@ class Server
 						nowPlaying = cancion.nombre;
 						timeLeft = cancion.timeLeft;
 						totalSongTime = cancion.timeLeft;
-					} else if(jumpPattern.matcher(message).matches()) {
-						Integer number = Integer.parseInt(jumpPattern.matcher(message).group(1));
+					} else if(jumpMatch.matches()) {
+						Integer number = Integer.parseInt(jumpMatch.group(1));
 						if(number == 0) {
 							timeLeft = totalSongTime;
 						} else {
@@ -127,7 +144,7 @@ class Server
 							}
 						}
 					} else if(historyPattern.matcher(message).matches()) {
-						sendUDPMessage("Esta es la lista de comandos", IP);
+						sendUDPMessage("Esta es la lista de comandos", IP); // Mostrar la lista de comandos (que aun hay que construir)
 					}
 				}
 			}
